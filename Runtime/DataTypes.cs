4f02248cb16d140ffe5e593cd3ddd08ff3e51e0f
Oliver Morland <oliversimonedward@hotmail.co.uk>
2024-07-02 11:57:23 -0400

    Started working on sample scene

---
+++ Runtime/DataTypes.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+namespace OpenAIAssistantsApi
+{
+    public struct ChatMessage
+    {
+        public string role;
+        public string content;
+    }
+
+}
+
---
+++ Runtime/OpenAIAssistant.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OpenAIAssistantsApi
+{
+    public class OpenAIAssistant : MonoBehaviour
+    {
+        public void AskAssistant(string message)
+        {
+
+        }
+    }
+}
