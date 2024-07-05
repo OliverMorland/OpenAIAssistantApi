e090a75909535f8682e44585f4f0a446118c3122
Oliver Morland <oliversimonedward@hotmail.co.uk>
2024-07-05 09:24:12 -0400

    Created a setup function

--- Tests/PlayMode/OpenAIAssistantTests.cs
+++ Tests/PlayMode/OpenAIAssistantTests.cs
@@ -9,13 +9,32 @@ namespace Tests
 {
     public class OpenAIAssistantTests
     {
+        [SetUp]
+        public void Setup()
+        {
+            OpenAIAssistant openAIAssistant = new OpenAIAssistant();
+            openAIAssistant.OnResponseRecieved.AddListener(OnOpenAIAssistantResponse);
+        }
+
+        void OnOpenAIAssistantResponse(string response)
+        {
+
+        }
+
         [UnityTest]
         public IEnumerator CreateNewThreadTest()
         {
             GameObject newAssistant = new GameObject();
             newAssistant.AddComponent<OpenAIAssistant>();
             OpenAIAssistant openAIAssistant = new OpenAIAssistant();
-            yield return null;
+            openAIAssistant.AskAssistant("Hello");
+            yield return new WaitForSeconds(5f);
+
+        }
+
+        void OnAssistantResponse()
+        {
+
         }
     }
 }
